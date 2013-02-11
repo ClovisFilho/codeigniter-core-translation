5e10b47b7e5fbd24a95f08f61bb3a5aac1c27cc0
Croves <me@crov.es>
2013-02-10 22:48:25 -0200

    File translated

---
+++ db_lang.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Translated file english > portuguese (brazil)
+ *
+ * @link https://github.com/ClovisFilho/codeigniter-core-translation
+ * @copyright Copyright (c) 2013, Clovis Filho <me@crov.es>
+ */
+
+$lang['db_invalid_connection_str'] = 'Impossível determinar as configurações do banco de dados com base na string de conexão informada.';
+$lang['db_unable_to_connect'] = 'Impossível conectar ao servidor de banco de dados com as configurações informadas.';
+$lang['db_unable_to_select'] = 'Impossível selecionar o banco de dados especificado: %s';
+$lang['db_unable_to_create'] = 'Impossível criar o banco de dados solicitado: %s';
+$lang['db_invalid_query'] = 'A query requisitada não é válida.';
+$lang['db_must_set_table'] = 'Você deve informar a tabela a ser usada com a sua query.';
+$lang['db_must_use_set'] = 'Você deve definir o método "SET" em sua query.';
+$lang['db_must_use_index'] = 'Você deve especificar um índice que combine com as atualizações em lote.';
+$lang['db_batch_missing_index'] = 'Uma ou mais colunas enviadas está sem o índice específico.';
+$lang['db_must_use_where'] = 'Comandos do tipo "UPDATES" não são permitidos a menos que contenham a cláusula "WHERE".';
+$lang['db_del_must_use_where'] = 'Comandos do tipo "DELETE" não são permitidos a menos que contenham a cláusula "WHERE" ou "LIKE".';
+$lang['db_field_param_missing'] = 'Para buscar os campos solicitados é necessário informar o nome da tabela como parâmetro.';
+$lang['db_unsupported_function'] = 'Esta ação não está disponível para o banco de dados que você está utilizando.';
+$lang['db_transaction_failure'] = 'Falha na transação: Rollback executado.';
+$lang['db_unable_to_drop'] = 'Impossível apagar o banco de dados solicitado.';
+$lang['db_unsuported_feature'] = 'Ação não suportada para o banco de dados em uso.';
+$lang['db_unsuported_compression'] = 'O formato de compressão de arquivos que você escolheu não é suportado pelo servidor.';
+$lang['db_filepath_error'] = 'Impossível escrever no arquivo que você enviou.';
+$lang['db_invalid_cache_path'] = 'O caminho para cache que você informou não é valido ou não possui as permissões necessárias.';
+$lang['db_table_name_required'] = 'O nome da tabela é necessário para esta operação';
+$lang['db_column_name_required'] = 'O nome da coluna é necessário para esta operação.';
+$lang['db_column_definition_required'] = 'A definição da coluna é necessária para esta operação.';
+$lang['db_unable_to_set_charset'] = 'Impossível definir a codificação de caracteres: %s';
+$lang['db_error_heading'] = 'Um erro de banco de dados ocorreu.';
+
+/* End of file db_lang.php */
+/* Location: ./system/language/english/db_lang.php */
