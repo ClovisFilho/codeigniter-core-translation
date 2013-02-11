c8a9aa7fe9fa4073278ad18240af411acb692d42
Croves <me@crov.es>
2013-02-10 22:34:20 -0200

    File translated using wikipedia as guide

---
+++ date_lang.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Translated file english > portuguese (brazil)
+ * http://pt.wikipedia.org/wiki/Tempo_Universal_Coordenado
+ *
+ * @link https://github.com/ClovisFilho/codeigniter-core-translation
+ * @copyright Copyright (c) 2013, Clovis Filho <me@crov.es>
+ */
+
+$lang['date_year'] = "Ano";
+$lang['date_years'] = "Anos";
+$lang['date_month'] = "Mês";
+$lang['date_months'] = "Meses";
+$lang['date_week'] = "Semana";
+$lang['date_weeks'] = "Semanas";
+$lang['date_day'] = "Dia";
+$lang['date_days'] = "Dias";
+$lang['date_hour'] = "Hora";
+$lang['date_hours'] = "Horas";
+$lang['date_minute'] = "Minuto";
+$lang['date_minutes'] = "Minutos";
+$lang['date_second'] = "Segundo";
+$lang['date_seconds'] = "Segundos";
+
+$lang['UM12']	= '(UTC -12:00) Horário de Baker e Howland';
+$lang['UM11']	= '(UTC -11:00) Horário de Midway,Samoa e Pago Pago';
+$lang['UM10']	= '(UTC -10:00) Horário de Havaí';
+$lang['UM95']	= '(UTC -9:30) Horário de marquesas';
+$lang['UM9']	= '(UTC -9:00) Horário de alaska';
+$lang['UM8']	= '(UTC -8:00) Horário de Los Angeles';
+$lang['UM7']	= '(UTC -7:00) Horário de Phoenix';
+$lang['UM6']	= '(UTC -6:00) Horário da Cidade do México';
+$lang['UM5']	= '(UTC -5:00) Horário de Nova York';
+$lang['UM45']	= '(UTC -4:30) Horário de Caracas';
+$lang['UM4']	= '(UTC -4:00) Horário Padrão do Atlantico';
+$lang['UM35']	= '(UTC -3:30) Horário Padrão de Newfoundland';
+$lang['UM3']	= '(UTC -3:00) Horário de Brasilia e Buenos Aires';
+$lang['UM2']	= '(UTC -2:00) Horário do Centro-Atlântico';
+$lang['UM1']	= '(UTC -1:00) Horário de Açores';
+$lang['UTC']	= '(UTC) Meridiano de Greenwich';
+$lang['UP1']	= '(UTC +1:00) Horário da Europa Central';
+$lang['UP2']	= '(UTC +2:00) Horario da África Central';
+$lang['UP3']	= '(UTC +3:00) Horário de Moscovo';
+$lang['UP35']	= '(UTC +3:30) Horário Padrão do Irã';
+$lang['UP4']	= '(UTC +4:00) Horário Padrão do Azerbaijão';
+$lang['UP45']	= '(UTC +4:30) Horário de Afeganistão';
+$lang['UP5']	= '(UTC +5:00) Horário Padrão do Paquistão';
+$lang['UP55']	= '(UTC +5:30) Horário Padrão Indiano';
+$lang['UP575']	= '(UTC +5:45) Horário de Nepal';
+$lang['UP6']	= '(UTC +6:00) Horário Padrão de Bangladesh';
+$lang['UP65']	= '(UTC +6:30) Horário de Myanmar';
+$lang['UP7']	= '(UTC +7:00) Horário do Vietnam';
+$lang['UP8']	= '(UTC +8:00) Horário Ocidental Australiano';
+$lang['UP875']	= '(UTC +8:45) Horário Padrão Australiano Central';
+$lang['UP9']	= '(UTC +9:00) Horário Padrão do Japão';
+$lang['UP95']	= '(UTC +9:30) Horário Padrão Australiano Central';
+$lang['UP10']	= '(UTC +10:00) Horário Oriental Australiano';
+$lang['UP105']	= '(UTC +10:30) Horário de Ilha de Lord Howe';
+$lang['UP11']	= '(UTC +11:00) Horário da Micronésia';
+$lang['UP115']	= '(UTC +11:30) Horario de Ilha Norfolk';
+$lang['UP12']	= '(UTC +12:00) Horário Padrão da Nova Zelândia';
+$lang['UP1275']	= '(UTC +12:45) Horário das Ilhas Chatham';
+$lang['UP13']	= '(UTC +13:00) Horário das Ilhas Phoenix';
+$lang['UP14']	= '(UTC +14:00) Horário das Ilhas da Linha';
+
+
+/* End of file date_lang.php */
+/* Location: ./system/language/english/date_lang.php */
